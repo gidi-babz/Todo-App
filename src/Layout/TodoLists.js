52fd10763ddf4bc0a7c091c7d511bf9259af1ad2
Gidi Babz <tunex4ind@gmail.com>
2022-07-15 15:02:45 +0100

    new commit

--- src/Layout/TodoLists.js
+++ src/Layout/TodoLists.js
@@ -3,12 +3,15 @@ import classes from './TodoLists.module.css';
 const TodoLists = props => {
   const { newTodo } = props;
 
+  const removeTodoItemHandler = () => {
+    console.log('Clicked');
+  };
   return (
     <ul className={classes.lists}>
       {newTodo.map(todo => (
         <li key={todo.id}>
           <p>{todo.newTodoItem}</p>
-          <button>&times;</button>
+          <button onClick={removeTodoItemHandler}>&times;</button>
         </li>
       ))}
     </ul>
